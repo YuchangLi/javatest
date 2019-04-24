7614a996d9318838a05b0a65a4d2dde36509263d
liyuchang <648821884@qq.com>
2019-04-24 19:10:30 +0800

    'udpate'

---
+++ java11/src/main/java/com/java11/rxjava/gc/PrinrtGCTest.java
@@ -0,0 +1,23 @@
+package com.java11.rxjava.gc;
+
+public class PrinrtGCTest {
+  public Object instance = null;
+  private static int _1MB = 1024 * 1024;
+  private byte[] bigSize = new byte[2 * _1MB];
+
+  public static void testGC() {
+    PrinrtGCTest test1 = new PrinrtGCTest();
+    PrinrtGCTest test2 = new PrinrtGCTest();
+    test1.instance = test2;
+    test2.instance = test1;
+    test1 = null;
+    test2 = null;
+    // 强制JVM进行垃圾回收
+    System.gc();
+  }
+
+  public static void main(String[] args) {
+    testGC();
+    System.out.println("123");
+  }
+}
